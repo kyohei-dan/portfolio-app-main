2f83f59fc3cfb5ad93d0788d409e01a7c229226d
kyohei_dan <kyohey.dan@gmail.com>
2021-08-19 02:40:22 +0900

    Note-Appの追加

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -1,6 +1,7 @@
 import port1 from "../img/portImages/tool-app.png";
 import port2 from "../img/portImages/todo-app.png";
 import port3 from "../img/portImages/chat-app.png";
+import port4 from "../img/portImages/note-app.png";
 
 const portfolios = [
 	{
@@ -29,6 +30,15 @@ const portfolios = [
 		image: port3,
 		title: "Chat App",
 		lang: "React,Firebase",
+	},
+	{
+		id: 4,
+		category: "Note App",
+		link1: "https://note-app-beta.vercel.app/",
+		icon1: "Go",
+		image: port4,
+		title: "Note App",
+		lang: "React",
 	},
 ];
 
